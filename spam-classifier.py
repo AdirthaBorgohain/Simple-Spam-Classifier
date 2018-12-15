d4d9006f76c4f2a6f4e58bf1207cbdd3c40d1dd1
AdirthaBorgohain <adirthaborgohain@gmail.com>
2018-12-16 01:14:12 +0530

    initialize test and train set

--- spam-classifier.py
+++ spam-classifier.py
@@ -30,3 +30,22 @@ mails.rename(columns = {'v1': 'labels', 'v2': 'message'}, inplace = True)
 
 # map ham and spam to 0 and 1 respectively
 mails['label'] = mails['labels'].map({'ham': 0, 'spam': 1})
+
+# remove 'labels' column
+mails.drop(['labels'], axis = 1, inplace = True)
+
+totalMails = 4825 + 747
+trainIndex, testIndex = list(), list()
+
+for i in range(mails.shape[0]):
+    if np.random.uniform(0, 1) < 0.75:
+        trainIndex += [i]
+    else:
+        testIndex += [i]
+trainData = mails.loc[trainIndex]
+testData = mails.loc[testIndex]
+
+trainData.reset_index(inplace = True)
+trainData.drop(['index'], axis = 1, inplace = True)
+testData.reset_index(inplace = True)
+testData.drop(['index'], axis = 1, inplace = True)
